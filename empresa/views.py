49777bf77598ff6be24fb9dad0138cd86f578ebb
Patricio Gonzalez Leal <pgonzalezl@indap.cl>
2016-08-16 16:26:51 -0300

    pep8

--- empresa/views.py
+++ empresa/views.py
@@ -5,35 +5,40 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 
-#empresa/
+
 class EmpresaList(APIView):
+    serializer_class = EmpresaSerializer
 
-	serializer_class = EmpresaSerializer
+    def get(self, request, id=None, format=None):
+        if id is not None:
+            empresa = get_object_or_404(Empresa, pk=id)
+            todos = False
+        else:
+            empresa = Empresa.objects.all()
+            todos = True
+        respuesta = EmpresaSerializer(empresa, many=todos)
+        return Response(respuesta.data)
 
-	def get(self, request, id=None, format=None):
-		if id != None:
-			empresa = get_object_or_404(Empresa,pk=id)
-			todos = False
-		else:
-			empresa = Empresa.objects.all()
-			todos = True
-		
-		respuesta = EmpresaSerializer(empresa, many=todos)
-		return Response(respuesta.data)
+    def post(self, request, id=None, format=None):
+        serializer = EmpresaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-	def post(self, request, id=None, format=None):
-		serializer = EmpresaSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		else:
-			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	
 
 class EmpresaViewset(viewsets.ModelViewSet):
-	serializer_class = EmpresaSerializer
-	queryset = Empresa.objects.all()
-	#lookup_field = 'id'
+    serializer_class = EmpresaSerializer
+    queryset = Empresa.objects.all()
 
-#empresa_list = EmpresaViewset.as_view({'get':'list'})
-#empresa_detail = EmpresaViewset.as_view({'get':'retrieve'})
+# lookup_field = 'id'
+
+# empresa_list = EmpresaViewset.as_view({'get':'list'})
+# empresa_detail = EmpresaViewset.as_view({'get':'retrieve'})
